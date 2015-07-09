a88cb0181e473f92c4cd5a0443de3a72ab54a512
sunabove <sunabove@nate.com>
2015-07-10 01:29:19 +0900

    f

--- addressBook/src/addressBook/AddressBookProgram.java
+++ addressBook/src/addressBook/AddressBookProgram.java
@@ -3,6 +3,7 @@ package addressBook;
 public class AddressBookProgram {
 	
 	public static void main( String [] args ) {
+		// program starts here
 		AddressBook addressBook = new AddressBook();
 		
 		addressBook.run();
