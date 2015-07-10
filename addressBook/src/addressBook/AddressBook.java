6bde1d2e8b0e800446429551ca7ee677523f8703
sunabove <sunabove@nate.com>
2015-07-10 14:45:14 +0900

    f

--- addressBook/src/addressBook/AddressBook.java
+++ addressBook/src/addressBook/AddressBook.java
@@ -1,5 +1,8 @@
 package addressBook;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class AddressBook { 
@@ -29,6 +32,9 @@ public class AddressBook {
 			} else if( menuCode.equalsIgnoreCase( "I") ) {
 				Person p = insertPerson();
 				println( "The person insterted: " + p.toString() );
+				if( p != null ) {
+					saveAllPersonsOnFile();
+				}
 				this.enterToContinue();
 			} else if( inputNo > -1 ) {
 				printPersonInformation( inputNo );
@@ -40,11 +46,48 @@ public class AddressBook {
 				println();
 				enterToContinue();
 			}
-		} while(  ! menuCode.equalsIgnoreCase( "Q")  ); 
+		} while( ! menuCode.equalsIgnoreCase( "Q")  ); 
 		
 		println(); 
 		String msg = "Good bye!"; 
 		println( msg );
+	}
+	
+	public void saveAllPersonsOnFile() {
+		String msg = null ; 
+		FileOutputStream fos = null;
+		File file; 
+		try { 
+			file = new File("c:/tmp/addressBook.txt");
+			fos = new FileOutputStream(file);  
+			if( ! file.exists() ) {
+				file.createNewFile();
+			}  
+			Person [] persons = this.persons;
+			Person p ;
+			String newLine = "\n"; 
+			for( int i = 0, iLen = persons.length ; i < iLen ; i ++ ) {
+				p = persons[i];
+				if( p != null ) {
+					fos.write( ( "" + p.id + newLine ).getBytes() );
+					fos.write( ( p.name + newLine ).getBytes() );
+					fos.write( ( p.phoneNumber + newLine ).getBytes() );
+					fos.write( ( p.address + newLine ).getBytes() ); 
+				}
+			} 
+			fos.flush();
+			fos.close();  
+			msg = "Success: saved person information on the file." ;
+		} catch (IOException e) {
+			e.printStackTrace();
+			msg = "Error: cannot save persons information on the file." ; 
+		} 
+		
+		if( msg != null ) {
+			println();
+			println( msg );
+			println();
+		}
 	}
 	
 	public void printPersonInformation( int userId ) {
