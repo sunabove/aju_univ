e6e507e7dc6642ac5d48ad075183e99f83217ef1
sunabove <sunabove@nate.com>
2015-07-22 15:24:36 +0900

    f

--- AndroidViews/src/com/example/androidviews/AutoCompleteActivity.java
+++ AndroidViews/src/com/example/androidviews/AutoCompleteActivity.java
@@ -4,18 +4,30 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class AutoCompleteActivity extends Activity {
 	
 	TextView messageTv ; 
+	Button okBtn ; 
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_autocomplete);
 		
-		this.messageTv = (TextView) findViewById( R.id.test_message );
+		this.messageTv = (TextView) findViewById( R.id.auto_message );
+		this.okBtn = (Button) findViewById( R.id.auto_ok );
+		
+		this.okBtn.setOnClickListener( new OnClickListener() { 
+			@Override
+			public void onClick(View v) { 
+				finish();
+			} 
+		});
 	}
 
 	@Override
