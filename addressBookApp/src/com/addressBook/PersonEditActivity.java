410fdb7a42bf767b47fb6778ab1a70359cc33e7d
sunabove <sunabove@nate.com>
2015-07-24 16:52:43 +0900

    f

--- addressBookApp/src/com/addressBook/PersonEditActivity.java
+++ addressBookApp/src/com/addressBook/PersonEditActivity.java
@@ -7,6 +7,7 @@ import java.util.*;
 import android.app.*;
 import android.content.*;
 import android.os.*; 
+import android.text.format.DateFormat;
 import android.view.*;
 import android.view.View.*;
 import android.widget.*; 
@@ -16,6 +17,17 @@ public class PersonEditActivity extends PersonCommonActivity {
 	Button saveBtn ;
 	Button editBtn ; 
 	Button cancelBtn ;  
+	
+	EditText personNameEt ;
+	RadioButton personMaleRd ; 
+	RadioButton personFemaleRd ; 
+	EditText personBirthDateEt ; 
+	EditText personPhoneNoEt ;
+	EditText personAddrEt ;
+	EditText personMemoEt ; 
+	
+	ImageView personPhotoIv ; 
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +37,16 @@ public class PersonEditActivity extends PersonCommonActivity {
 		this.saveBtn = (Button) findViewById( R.id.edit_save_btn );
 		this.editBtn = (Button) findViewById( R.id.edit_edit_btn );
 		this.cancelBtn = (Button) findViewById( R.id.edit_cancel_btn );
+		
+		this.personNameEt = (EditText) findViewById( R.id.edit_person_name );
+		this.personBirthDateEt = (EditText) findViewById( R.id.edit_person_birth_date );
+		this.personPhoneNoEt = (EditText) findViewById( R.id.edit_person_phone_number );
+		this.personAddrEt = (EditText) findViewById( R.id.edit_person_address );
+		
+		this.personMaleRd = (RadioButton) findViewById( R.id.edit_person_gender_male );
+		this.personFemaleRd = (RadioButton) findViewById( R.id.edit_person_gender_female );
+		
+		this.personPhotoIv = (ImageView) findViewById(R.id.edit_person_photo );
 		
 		this.editBtn.setVisibility( View.GONE );
 		
@@ -49,6 +71,50 @@ public class PersonEditActivity extends PersonCommonActivity {
 	}
 	
 	public void savePerson() {
+		String name = this.personNameEt.getText().toString() ;
+		if( name == null || name.trim().length() < 1 ) {
+			Context context = this.getApplicationContext();
+			new AlertDialog.Builder(context)
+		    .setTitle("이름 입력")
+		    .setMessage("주소록을 추가하려면 이름을 입력하여야 합니다.")
+		    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+		        public void onClick(DialogInterface dialog, int which) {  
+		        }
+		     })
+		    .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+		        public void onClick(DialogInterface dialog, int which) {  
+		        }
+		     })
+		    .setIcon(android.R.drawable.ic_dialog_alert)
+		    .show();
+			
+			return ; 
+		}
+		
+		Person.Gender gender = this.personMaleRd.isChecked() ? Person.Gender.MALE : Person.Gender.FEMALE ;
+		Date birthDate = null ;
+		String birthDateStr = this.personBirthDateEt.getText().toString();
+		try {
+			SimpleDateFormat dateFormat =new SimpleDateFormat("yyyyMMDD");
+			birthDate = dateFormat.parse( birthDateStr );
+		} catch (Exception e) { 
+		}
+		String phoneNumber = this.personPhoneNoEt.getText().toString() ;
+		String address =this.personAddrEt.getText().toString();
+		String memo = this.personMemoEt.getText().toString();
+		
+		Person p = new Person();
+		p.name = name.trim() ;
+		p.gender = gender ; 
+		p.birthDate = birthDate ;
+		p.phoneNumber = phoneNumber ;
+		p.address = address ; 
+		p.memo = memo ; 
+		
+		AddressBook addressBook = AddressBook.getAddressBook();
+		
+		p.id = addressBook.persons.size() ; 
+		addressBook.persons.add( p );
 		
 	}
 	
@@ -56,10 +122,26 @@ public class PersonEditActivity extends PersonCommonActivity {
 		finish();
 	}
 	
-	public void viewList() {
-		
+	public void viewList() { 
 	}
 	
-	
+	public void showMessageDialog( String msg ) {
+		Context context = this.getApplicationContext();
+		new AlertDialog.Builder(context)
+	    .setTitle("Delete entry")
+	    .setMessage("Are you sure you want to delete this entry?")
+	    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+	        public void onClick(DialogInterface dialog, int which) { 
+	            // continue with delete
+	        }
+	     })
+	    .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+	        public void onClick(DialogInterface dialog, int which) { 
+	            // do nothing
+	        }
+	     })
+	    .setIcon(android.R.drawable.ic_dialog_alert)
+	     .show();
+	}  
 	
 }
