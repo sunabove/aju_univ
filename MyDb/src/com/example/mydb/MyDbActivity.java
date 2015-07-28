4e41a5209116c004a0c9a7830bc7d2b5cb6feef5
sunabove <sunabove@nate.com>
2015-07-28 17:40:44 +0900

    f

--- MyDb/src/com/example/mydb/MyDbActivity.java
+++ MyDb/src/com/example/mydb/MyDbActivity.java
@@ -6,11 +6,15 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MyDbActivity extends Activity {
+	
+	FeedReaderDbHelper mDbHelper  ;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_my_db);
+		
+		this.mDbHelper = new FeedReaderDbHelper( getApplicationContext() );; 
 	}
 
 	@Override
